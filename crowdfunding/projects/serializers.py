4d7261493e16ac96286a734117e6a9af741153a9
Behnaz Shojaei <behnaz.shojaei88@gmail.com>
2024-09-09 20:46:11 +0800

    wrote my first DRF serializer

---
+++ crowdfunding/projects/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from django.apps import apps
+
+# The ModelSerializer class already knows how to convert a model instance into JSON!
+class ProjectSerializer(serializers.ModelSerializer):
+    # The only customisation that we need to do is to tell it which model to convert, class Meta: block is doing
+    class Meta:
+        model = apps.get_model('projects.Project')
+        fields = '__all__'
+        # We tell our new serializer that its job is to serialize our Project model into JSON, and that it should include __all__ of the fields when it does so!
