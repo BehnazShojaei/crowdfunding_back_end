48ca9fbc31fc6c655396ebb23c503ff25a99b707
Behnaz Shojaei <behnaz.shojaei88@gmail.com>
2024-09-09 20:11:42 +0800

    wrote my first django model

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+# inherited our class from the built-in models.Model class
+class Project(models.Model):
 
-# Create your models here.
+#  Each of these is itself an instance of one of the classes that comes bundled with Django. These attributes tell Django what types of fields we want in our database table.
+# We can pass arguments to our model's field attributes to specify extra functionality
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    goal = models.IntegerField()
+    image = models.URLField()
+    is_open = models.BooleanField()
+    date_created = models.DateTimeField(auto_now_add=True)
