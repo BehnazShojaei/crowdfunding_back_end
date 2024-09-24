8e9ea86df134b6a222d27bee3fd1f53218fa6bf6
Behnaz <behnaz.shojaei88@gmail.com>
2024-09-24 19:14:20 +0800

    add/update project files

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -1,6 +1,10 @@
 from rest_framework import serializers
 from django.apps import apps
 
+class PledgeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = apps.get_model('projects.Pledge')
+        fields = '__all__'
 # The ModelSerializer class already knows how to convert a model instance into JSON!
 class ProjectSerializer(serializers.ModelSerializer):
     # The only customisation that we need to do is to tell it which model to convert, class Meta: block is doing
