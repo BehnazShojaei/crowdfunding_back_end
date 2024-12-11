59e2a367653f4fb553b49de1b2585fea5c09711c
Behnaz <behnaz.shojaei88@gmail.com>
2024-12-11 14:01:57 +0800

    Added cloudinary storage for media files

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -14,11 +14,16 @@ from pathlib import Path
 import os
 import dj_database_url
 from dotenv import load_dotenv
+load_dotenv()
 
-load_dotenv("../.env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'fallback-secret-key')
+DEBUG = os.getenv('DJANGO_DEBUG') != 'False'
+
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -40,7 +45,7 @@ DEBUG = os.environ.get(
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['*']
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = DEBUG
 
 
 # Application definition
@@ -58,6 +63,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'cloudinary',
+    'cloudinary_storage',
 ]
 
 REST_FRAMEWORK = {
@@ -153,8 +160,16 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
