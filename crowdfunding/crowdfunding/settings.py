47bc721e3bc320835c68e7766f8017968ae966ea
Behnaz <behnaz.shojaei88@gmail.com>
2024-12-13 01:30:39 +0800

    modified config for s3

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -175,7 +175,7 @@ AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = os.getenv('AWS_REGION_NAME')
 AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = 'public-read'
+# AWS_DEFAULT_ACL = '-read'
 AWS_QUERYSTRING_AUTH = False  # Makes URLs public
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',  # Caching for 1 day
@@ -184,5 +184,4 @@ AWS_S3_OBJECT_PARAMETERS = {
 # Set S3 as Default Storage for Media Files
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
-AWS_QUERYSTRING_AUTH = False  # Optional, makes URLs cleaner
+MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/'
