c02ec23b743f6f81eeedf70e1616975ad22cf932
Behnaz <behnaz.shojaei88@gmail.com>
2024-12-19 10:21:25 +0800

    edit and delete project tested

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -91,7 +91,8 @@ class ProjectDetail(APIView):
     def delete(self, request, pk):
         project = self.get_object(pk)
         project.delete()
-        return Response({"detail": "Project successfully deleted"},status=status.HTTP_204_NO_CONTENT)
+        return Response({"detail": "Project successfully deleted"},status=status.HTTP_200_OK)
+
 
 
 # this was my post without any logic!   
